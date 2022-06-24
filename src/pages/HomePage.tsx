98761c36e1c7c5c5dd06b71bd0dc2668a9a2120d
Tom Hughes <tomhughes1987@gmail.com>
2022-06-24 13:54:03 +0200

    spelling peak>peek

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,7 +7,7 @@ export default function HomePage() {
       <h1>This page is under consruction</h1>
     </div>
     <div>
-      <h3>A sneak peak:</h3>
+      <h3>A sneak peek:</h3>
       <img src='sneakpeak1.PNG' />
       <img src='sneakpeak2.PNG' />
       <img src='sneakpeak3.PNG' />
