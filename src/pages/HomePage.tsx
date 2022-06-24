2c12a17ff7cfa8d10f13ece25e70a654cacb9681
Tom Hughes <tomhughes1987@gmail.com>
2022-06-24 13:52:53 +0200

    add sneak peak images

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <>
+    <div>
+      <h1>This page is under consruction</h1>
+    </div>
+    <div>
+      <h3>A sneak peak:</h3>
+      <img src='sneakpeak1.PNG' />
+      <img src='sneakpeak2.PNG' />
+      <img src='sneakpeak3.PNG' />
+
+    </div>
+    </>
+
+
+  )
+}
