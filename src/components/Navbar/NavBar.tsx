d8b09dd227b4f7b867afff2bd0cde668ef79df17
Tom Hughes <tomhughes1987@gmail.com>
2022-06-25 19:20:45 +0200

    add a navbar

---
+++ src/components/Navbar/NavBar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './NavBar.css'
+
+
+export default function NavBar() {
+  return (
+<>
+    <div className='Container-Navbar'>
+        <p className='NavBar-Link'>About</p>
+        <p className='NavBar-Link'>GitHub Stats</p>
+        <p className='NavBar-Link'>Music Player</p>
+        <p className='NavBar-Link'>Skills</p>
+    </div>
+  </>
+  )
+}
