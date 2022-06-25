27e53b79790b76e501d0dcb33ac19adc1c9f983e
Tom Hughes <tomhughes1987@gmail.com>
2022-06-25 19:22:47 +0200

    import <navbar /> + <titletag /> onto <homepage/>

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import MainCursor from '../components/MainCursor/MainCursor'
+import NavBar from '../components/Navbar/NavBar'
+import TitleTag from '../components/TitleTag/TitleTag'
 import WorkInProgress from '../components/WorkInProgress/WorkInProgress'
 import './HomePage.css'
 
 export default function HomePage() {
   return (
     <>
+
+    {/* <NavBar /> */}
+    <TitleTag />
     <MainCursor />
     
     <div className='EntireSplashScreen'></div>
