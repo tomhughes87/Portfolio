fb2b41f9f3174b3f9756ed6edd192954494a5944
Tom Hughes <tomhughes1987@gmail.com>
2022-06-25 10:17:02 +0200

    add a sneakpeek component as placeholder/demo

---
+++ src/components/WorkInProgress/WorkInProgress.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function WorkInProgress() {
+  return (
+    <>
+    <div>
+    <h1>This page is under consruction</h1>
+    </div>
+    <div>
+        <h3>A sneak peek:</h3>
+        <img src='sneakpeak1.PNG' />
+        <img src='sneakpeak2.PNG' />
+        <img src='sneakpeak3.PNG' />
+    </div>
+    </>
+  )
+}
