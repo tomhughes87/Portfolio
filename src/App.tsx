eea9e6caa127fc30736563d941516233766a62bd
Tom Hughes <tomhughes1987@gmail.com>
2022-06-25 19:20:23 +0200

    add splashscreen to app.js

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/HomePage';
+import SplashScreen from './components/SplashScreen/SplashScreen';
 
 function App() {
+
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    // <SplashScreen />
+    // <>
+   <HomePage />
+    // </>
   );
 }
 
